910dfec7728e3eeed18e2f3c7184be54cdd06ffd
himalagunathilaka <himala.22@cse.mrt.ac.lk>
2025-04-16 20:17:35 +0530

    chore(CRUD user)
    Now new user can be added or find a user by username

--- backend/Aththatu/users/urls.py
+++ backend/Aththatu/users/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('profile/<str:username>/', views.get_profile_by_username, name='get_profile'),
+    path('createprofile/', views.create_user, name='create_user')
 ]
